5fa5aa84247fc338f93a30c45ffea1bb2c0080f9
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 21:02:31 +0700

    Update tokiEvent.js

--- system/toki/tokiEvent.js
+++ system/toki/tokiEvent.js
@@ -1,1 +1,16 @@
+const events = [
 
+"Seekor naga muncul di gunung utara.",
+"Harta karun ditemukan di hutan.",
+"Dungeon misterius terbuka.",
+"Monster langka muncul."
+
+]
+
+function randomEvent(){
+
+return events[Math.floor(Math.random()*events.length)]
+
+}
+
+module.exports = { randomEvent }
