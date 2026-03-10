30074b01c8ca7c360c6712ab58fec623570a32d1
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 21:00:38 +0700

    Update tokiNPC.js

--- system/toki/tokiNPC.js
+++ system/toki/tokiNPC.js
@@ -1,1 +1,31 @@
+const fs = require("fs")
+const path = require("path")
 
+const db = path.join(process.cwd(),"rpg/data/players.json")
+
+function playerStatus(user){
+
+try{
+
+const data = JSON.parse(fs.readFileSync(db))
+
+const p = data[user]
+
+if(!p) return "Kamu belum membuat karakter."
+
+return `Statusmu
+
+HP: ${p.hp}
+Attack: ${p.attack}
+Defense: ${p.defense}
+Gold: ${p.gold}`
+
+}catch{
+
+return "Toki tidak bisa membaca data player."
+
+}
+
+}
+
+module.exports = { playerStatus }
