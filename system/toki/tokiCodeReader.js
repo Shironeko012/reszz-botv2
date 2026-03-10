b13f47041a6f9a026eb2a6309f886c9f32d0d5b2
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 21:11:47 +0700

    Update tokiCodeReader.js

--- system/toki/tokiCodeReader.js
+++ system/toki/tokiCodeReader.js
@@ -1,1 +1,56 @@
+const fs = require("fs")
+const path = require("path")
 
+function readDir(dir){
+
+let results = []
+
+const list = fs.readdirSync(dir)
+
+list.forEach(file=>{
+
+const filePath = path.join(dir,file)
+
+const stat = fs.statSync(filePath)
+
+if(stat && stat.isDirectory()){
+
+results = results.concat(readDir(filePath))
+
+}else{
+
+if(file.endsWith(".js")){
+results.push(filePath)
+}
+
+}
+
+})
+
+return results
+
+}
+
+function readBotCode(){
+
+const root = process.cwd()
+
+const files = readDir(root)
+
+let code = ""
+
+files.forEach(f=>{
+
+try{
+
+code += fs.readFileSync(f,"utf8")+"\n"
+
+}catch{}
+
+})
+
+return code.slice(0,50000)
+
+}
+
+module.exports = { readBotCode }
