aa8237efca5e30ff7643ffdb9d48831940f2251e
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 21:03:53 +0700

    Update tokiDebugger.js

--- system/toki/tokiDebugger.js
+++ system/toki/tokiDebugger.js
@@ -1,1 +1,19 @@
+const fs = require("fs")
 
+function readError(){
+
+try{
+
+const log = fs.readFileSync("logs/error.log","utf8")
+
+return log.slice(-500)
+
+}catch{
+
+return "Tidak ada error."
+
+}
+
+}
+
+module.exports = { readError }
