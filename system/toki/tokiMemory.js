92469407ba4df1b8b8b03d68aa17af101ddce9d7
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 20:59:03 +0700

    Update tokiMemory.js

--- system/toki/tokiMemory.js
+++ system/toki/tokiMemory.js
@@ -1,1 +1,37 @@
+const fs = require("fs")
+const path = require("path")
 
+const db = path.join(process.cwd(),"database/tokiMemory.json")
+
+if(!fs.existsSync(db)){
+fs.writeFileSync(db,JSON.stringify({}))
+}
+
+function getMemory(user){
+
+const data = JSON.parse(fs.readFileSync(db))
+
+return data[user] || []
+
+}
+
+function saveMemory(user,msg){
+
+const data = JSON.parse(fs.readFileSync(db))
+
+if(!data[user]) data[user] = []
+
+data[user].push(msg)
+
+if(data[user].length > 10){
+data[user].shift()
+}
+
+fs.writeFileSync(db,JSON.stringify(data,null,2))
+
+}
+
+module.exports = {
+getMemory,
+saveMemory
+}
