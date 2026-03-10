273d30b294114417ca8736dd0e7294384f24d287
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 18:59:57 +0700

    Update dl.js

--- commands/downloader/dl.js
+++ commands/downloader/dl.js
@@ -14,13 +14,21 @@ text:"📥 Kirim link video\n\n.dl https://youtube.com/watch?v=xxxx"
 })
 }
 
+const url = args[0]
+
+if(!url.includes("youtube.com") && !url.includes("youtu.be")){
+return sock.sendMessage(m.id,{
+text:"❌ Link harus dari YouTube"
+})
+}
+
 try{
 
 await sock.sendMessage(m.id,{
-text:"⏬ Downloading video..."
+text:"⏬ Mengunduh video..."
 })
 
-const data = await video(args[0])
+const data = await video(url)
 
 const msg = await sock.sendMessage(m.id,{
 video:{ url:data.file },
@@ -28,12 +36,11 @@ caption:`🎬 ${data.title}`
 })
 
 selfDestruct(sock,m.id,msg.key)
-
 cleanTmp(data.file)
 
 }catch(err){
 
-console.log(err)
+console.log("DL ERROR:",err)
 
 sock.sendMessage(m.id,{
 text:"❌ Gagal download video"
