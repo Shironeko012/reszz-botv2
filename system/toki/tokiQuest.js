0a1612876af57c6a04a139288afa91233e4e7751
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 21:01:41 +0700

    Update tokiQuest.js

--- system/toki/tokiQuest.js
+++ system/toki/tokiQuest.js
@@ -1,1 +1,24 @@
+const quests = [
 
+"Bunuh 3 monster hutan",
+"Kumpulkan 5 kayu",
+"Kalahkan slime raksasa",
+"Cari artefak kuno",
+"Buru serigala liar"
+
+]
+
+function generateQuest(){
+
+const quest = quests[Math.floor(Math.random()*quests.length)]
+
+const reward = Math.floor(Math.random()*200)+50
+
+return {
+quest,
+reward
+}
+
+}
+
+module.exports = { generateQuest }
