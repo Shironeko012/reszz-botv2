2dfa7b474045c06f218c0c7f075b0e987d45c327
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 21:13:31 +0700

    Update tokiDevAI.js

--- system/toki/tokiDevAI.js
+++ system/toki/tokiDevAI.js
@@ -1,1 +1,34 @@
+const axios = require("axios")
+const { readBotCode } = require("./tokiCodeReader")
 
+async function analyzeBug(error){
+
+const code = readBotCode()
+
+try{
+
+const res = await axios.post(
+"https://api.itsrose.rest/chatGPT/turbo",
+{
+prompt:`
+Analisa bug berikut:
+
+ERROR:
+${error}
+
+CODE:
+${code}
+`
+})
+
+return res.data.result
+
+}catch{
+
+return "Toki gagal menganalisa bug."
+
+}
+
+}
+
+module.exports = { analyzeBug }
