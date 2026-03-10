7b91876c6a67971aedcb19bf6924faa2c96f9ead
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 20:56:13 +0700

    Update tokiController.js

--- system/toki/tokiController.js
+++ system/toki/tokiController.js
@@ -1,1 +1,25 @@
+const { askToki } = require("./tokiEngine")
 
+module.exports = async function tokiController(sock,m){
+
+try{
+
+let text = m.text || ""
+
+if(!text.toLowerCase().includes("toki")) return
+
+text = text.replace(/toki/i,"").trim()
+
+const reply = await askToki(m.sender,text)
+
+await sock.sendMessage(m.id,{
+text:reply
+})
+
+}catch(err){
+
+console.log("TOKI CONTROLLER ERROR:",err)
+
+}
+
+}
