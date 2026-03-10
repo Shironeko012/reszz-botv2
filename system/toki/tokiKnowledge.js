1e8efedce3fc1b33c6915b4d9b830b959511bd27
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 21:03:05 +0700

    Update tokiKnowledge.js

--- system/toki/tokiKnowledge.js
+++ system/toki/tokiKnowledge.js
@@ -1,1 +1,14 @@
+const fs = require("fs")
+const path = require("path")
 
+function listCommands(){
+
+const dir = path.join(process.cwd(),"commands")
+
+const files = fs.readdirSync(dir)
+
+return "Command bot:\n\n"+files.join("\n")
+
+}
+
+module.exports = { listCommands }
