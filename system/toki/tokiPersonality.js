9b98949aa1c2e597847e59106c6df420d9787082
Shironeko012 <153088951+Shironeko012@users.noreply.github.com>
2026-03-10 21:04:35 +0700

    Update tokiPersonality.js

--- system/toki/tokiPersonality.js
+++ system/toki/tokiPersonality.js
@@ -1,1 +1,25 @@
+function personality(user,message){
 
+const owner = "reszz"
+
+let intro = ""
+
+if(user.toLowerCase().includes(owner)){
+intro = "Reszz… Toki selalu di sini untukmu."
+}else{
+intro = "Petualang."
+}
+
+if(message.includes("siapa kamu")){
+return "Aku Asuma Toki. Penjaga dunia Reszz."
+}
+
+if(message.includes("bantu")){
+return `${intro} Apa yang bisa Toki bantu?`
+}
+
+return `${intro} Toki mendengarkan.`
+
+}
+
+module.exports = { personality }
